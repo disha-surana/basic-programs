219523b0aed3aefe19dc4efa7aa260737913f7c6
DISHA SURANA <dishasurana11@gmail.com>
2018-10-02 20:06:25 +0530

    Create primeNo.cpp

---
+++ primeNo.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int n,flag=0;
+    cout<<"Enter number 'n' : ";
+    cin>>n;
+    
+    if(n==1){
+       cout<<"\n 1 is neither prime nor composite.";
+        return 0;
+    }
+       
+    for(int i=2; i <= sqrt(n); i++){
+        
+        if(n%i==0)
+        {   
+            flag=1;
+            break;
+        }
+        
+    }
+    
+    if(flag==0)
+        cout<<"\n"<<n<<" is a prime number.";
+    else
+         cout<<"\n"<<n<<" is not a prime number.";
+
+    return 0;
+}
